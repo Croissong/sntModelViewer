4f6bf42ecbbff381e510158e44107e27490bd29f
Jan Möller <jm@iogroup.net>
2016-03-14 10:29:11 +0100

    wip

--- src/redux/modules/editor.js
+++ src/redux/modules/editor.js
@@ -1,4 +1,4 @@
-import { combineReducers } from 'redux-immutable';
+import { combineReducers } from 'redux';
 import { formReducer, actionTypes } from 'react-redux-form';
 
 // ------------------------------------
